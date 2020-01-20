468a97c16a044ff9e2cb88a57a19b11d8e25f423
irungumargaret <sairmargaret44@gmail.com>
2020-01-20 09:29:38 +0300

    final commit

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -1,12 +1,10 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
-  name: 'dateCount'
+  name: "dateCount"
 })
 export class DateCountPipe implements PipeTransform {
-
   transform(value: any, ...args: any[]): any {
     return null;
   }
-
 }
